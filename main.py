7656d94a213c9356d8fa5dc7bc821e5b11845f23
Elias Serra <>
2023-03-22 00:45:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,83 @@
+# Operações básicas
+
+#num1 = 20
+#num2 = 30 
+
+#n1 = 'Elias'
+
+#n2 = 'Serra'
+
+#total = n1 + n2
+#print(total)
+
+#num1 = int ( input('Insira um número: '))
+#num2 = int ( input('Insira o segundo número: '))
+
+#total = num1 + num2
+
+#print (f"Valor da soma é {total}")
+
+#Exemplo de sálario 
+
+#salario = int (input("Insira seu salario: "))
+
+#if salario <= 2000:
+  #aumento1 = salario + (salario*0.2)
+  #print(f"Seu salario aumentou 20% {aumento1}")
+#elif salario >= 5000: # aumento de 10%
+#  aumento2 = salario + (salario*0.1)
+  #print(f"Seu salario sofreu um aumento de 10% totalizndo {aumento2}")
+
+  # boletin nota1 e nota2 ,calcular a média, numero de faltas, se a méadia for <= a 6 aprovado se não reprovado 
+
+
+#nota1 = int(input("Insira nota1: "))
+#nota2 = int(input("Insira nota1: "))
+
+#media = (nota1 + nota2)/2
+
+#if media >= 6:
+
+#  print(f"Aluno Aprovado!")
+
+#else media >= 6:
+#
+  #print(f"reprovado!")
+
+#print(f"Valor das notas {total}")
+
+#pedir nome, idade, se for maior ou igual a 18 maior de idade, se não menor de idade, nome e idade 
+
+#print(" --------Cadastro-------")
+
+#nome = input (f"Insira seu nome: ")
+
+#idade = int(input(f"Insira sua idade: "))
+
+#i#f idade >=18:
+ # print(f"Olá {nome} você é maior com {idade} anos")
+#else:
+  #print(f" Olá {nome} você é menor  com {idade} anos")
+#case tabuaparada
+#
+#
+#
+#
+#parada =""
+#while True:
+  #fruta = input("insira o nome de uma fruta: ")
+ # para = input("Deseja continuar S/N: ").upper()
+  #if parada  != "S":
+  #  break
+
+#Nome
+#idade
+#Estado
+#Parada FINAL
+
+print input("Insira seu nome: ")
+
+print("Insira sua idade: ")
+
+print("Insira seu estado: ")
+
